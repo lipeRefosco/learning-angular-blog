1bf59e712a0a506c96551459cf42e4326f627a2b
lipeRefosco] <luisfelipe.refosco@hotmail.com>
2023-10-15 13:08:00 -0300

    fix: put the for loop on right place

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -6,16 +6,17 @@ import { Post } from 'src/app/types/Post';
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent implements OnInit{
+export class PostsComponent implements OnInit {
 
   posts: Post[] = []
 
   ngOnInit(): void {
-    this.posts = Array.from({length: 3}, (_,k) => ({
+    this.posts = Array.from({length: 5}, (_,i) => ({
       date: '2023-10-15',
-      title: `Post ${k}`,
+      title: `Post ${i}`,
       img: 'https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YWJzdHJhY3R8ZW58MHx8MHx8fDA%3D&w=1000&q=80',
-      desc: `Small text for post ${k} card...`
+      desc: `Small text for post ${i} card...`
     }));
+    console.log(this.posts)
   }
 }
