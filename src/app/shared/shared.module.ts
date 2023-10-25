1ed3d76cc0cb59a937faf29028976d2a16f7a127
lipeRefosco] <luisfelipe.refosco@hotmail.com>
2023-10-25 01:14:02 -0300

    refact: creating a component to Date and sharing with home and post Modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageTitleComponent } from '../components/page-title/page-title.component';
+import { DateComponent } from '../components/date/date.component';
 
 
 
 @NgModule({
   declarations: [
     PageTitleComponent,
+    DateComponent
   ],
   exports: [
-    PageTitleComponent
+    PageTitleComponent,
+    DateComponent
   ],
   imports: [
     CommonModule
